d4bf6a793653920e13384729fa05cb139d9144cb
Chinmay <chinmay.singh@shipglobal.in>
2024-12-05 15:24:23 +0530

    added more components

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, useState } from "react";
 
 const Badge = lazy(() => import("franchise/badge"));
 const Button = lazy(() => import("franchise/button"));
@@ -8,26 +8,83 @@ const Label = lazy(() => import("franchise/label"));
 const TextArea = lazy(() => import("franchise/textArea"));
 const Calendar = lazy(() => import("franchise/calendar"));
 const ErrorMessage = lazy(() => import("franchise/ErrorMessage"));
+const AnnouncementBar = lazy(() => import("franchise/AnnouncementBar"));
+const ButtonWithIcon = lazy(() => import("franchise/ButtonWithIcon"));
+const FileInput = lazy(() => import("franchise/FileInput"));
+const FileInputEditable = lazy(() => import("franchise/FileInputEditable"));
+const InfoBox = lazy(() => import("franchise/InfoBox"));
+const LoadingButton = lazy(() => import("franchise/LoadingButton"));
+const LoadingIconButton = lazy(() => import("franchise/LoadingIconButton"));
+const LoadingScreen = lazy(() => import("franchise/LoadingScreen"));
+const PhoneCode = lazy(() => import("franchise/PhoneCode"));
+const ProfileIcon = lazy(() => import("franchise/ProfileIcon"));
+
 function App() {
+  const Announcement = ["This is an annoucementbar component"];
+  const [fileId, setFileId] = useState<string>("");
+  const [fileIdEdit, setFileIdEdit] = useState<string>("");
   return (
-    <div className="grid grid-cols-2 gap-4 mt-4 ml-4">
-      <Badge variant="success" className="max-w-max">
-        This is badge component
-      </Badge>
-      <Button variant="default" className="max-w-max">
-        This is button component
-      </Button>
-      <Card className="max-w-max mx-0">This is card component</Card>
-      <Input
-        type="text"
-        className="max-w-max"
-        placeholder="This is input component"
-      />
-      <Label className="max-w-max">This is label component</Label>
-      <TextArea
-        className="max-w-max"
-        placeholder="This is text area component"
-      />
+    <>
+      <AnnouncementBar announcements={Announcement} />
+      <div className="grid grid-cols-2 mt-16 gap-4  ml-4">
+        <Badge variant="success" className="max-w-max">
+          This is badge component
+        </Badge>
+        <Button variant="default" className="max-w-max">
+          This is button component
+        </Button>
+        <Card className="max-w-max mx-0">This is card component</Card>
+        <Input
+          type="text"
+          className="max-w-max"
+          placeholder="This is input component"
+        />
+        <Label className="max-w-max">This is label component</Label>
+        <TextArea
+          className="max-w-max"
+          placeholder="This is text area component"
+        />
+        <ErrorMessage
+          error="This is the error message component"
+          className="max-w-max max-h-min"
+        />
+        <ButtonWithIcon
+          text="This is button with icon component"
+          variant="secondary"
+          iconName="bx:bx-home"
+          className="max-w-max"
+        />
+        <FileInput
+          label="This is file input component"
+          required={true}
+          className="max-w-max"
+          name="file"
+          setFileId={setFileId}
+        />
+        <div className="max-w-72">
+          <FileInputEditable
+            label="This is file input editable component"
+            required={true}
+            name="fileedit"
+            fileId={fileIdEdit}
+            setFileId={setFileIdEdit}
+          />
+        </div>
+        <InfoBox title="This is info box component" className="max-w-max" />
+
+        <LoadingButton loading={true} text="this is loading button" />
+        <LoadingIconButton
+          loading={true}
+          className="max-w-72"
+          text="This is loading icon button"
+        />
+        <div>
+          This is loading screen component
+          <LoadingScreen />
+        </div>
+        <PhoneCode />
+        <ProfileIcon text="MicroFC" className="max-w-max" />
+      </div>
       <Calendar
         mode="single"
         className="max-w-max"
@@ -38,8 +95,7 @@ function App() {
         fromYear={1900}
         toYear={2012}
       />
-      <ErrorMessage error="This is the error message component" />
-    </div>
+    </>
   );
 }
 
